7d9b8541b9c7da0ec1d1be4a3adcddd6da4c6c7c
altaf <altaf98khan66@gmail.com>
2023-04-12 22:38:10 +0530

    Add: To validate password having atleast 1 Upper Case

--- src/main/java/com/bridgelabz/userregistrationproblem/UserRegistrationProblem.java
+++ src/main/java/com/bridgelabz/userregistrationproblem/UserRegistrationProblem.java
@@ -8,16 +8,15 @@ public class UserRegistrationProblem {
 
 	public static void main(String[] args) {
 
-		String regex = "^.{8,}$";
+		String regex = "^(?=.*[A-Z]).{8,}$";
 
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter A Password: ");
+		System.out.println("Enter a password having atleast one uppercase : ");
 		String password = sc.nextLine();
 
 		Pattern pattern = Pattern.compile(regex);
 		Matcher matcher = pattern.matcher(password);
 		boolean result = matcher.matches();
-
 		if (result) {
 			System.out.println("You have entered a Valid Password");
 		} else {
