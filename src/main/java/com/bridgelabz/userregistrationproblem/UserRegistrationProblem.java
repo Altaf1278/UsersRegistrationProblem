ccba0c997d15f7d41893041638fabb44f1c897d9
altaf <altaf98khan66@gmail.com>
2023-04-12 22:28:22 +0530

    Add: To validate password  entered by user

--- src/main/java/com/bridgelabz/userregistrationproblem/UserRegistrationProblem.java
+++ src/main/java/com/bridgelabz/userregistrationproblem/UserRegistrationProblem.java
@@ -8,20 +8,20 @@ public class UserRegistrationProblem {
 
 	public static void main(String[] args) {
 
-		String regex = "^[1-9][0-9]\\s[1-9][0-9]{9}";
+		String regex = "^.{8,}$";
 
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter Mobile Number: ");
-		String mobileNO = sc.nextLine();
+		System.out.println("Enter A Password: ");
+		String password = sc.nextLine();
 
 		Pattern pattern = Pattern.compile(regex);
-		Matcher matcher = pattern.matcher(mobileNO);
+		Matcher matcher = pattern.matcher(password);
 		boolean result = matcher.matches();
 
 		if (result) {
-			System.out.println("Valid Mobile Number");
+			System.out.println("You have entered a Valid Password");
 		} else {
-			System.out.println("Invalid Mobile Number");
+			System.out.println("Invalid Password");
 		}
 
 	}
