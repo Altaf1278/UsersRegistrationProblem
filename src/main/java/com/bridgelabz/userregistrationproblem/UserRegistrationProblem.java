b94ffd39e6c24fe0562907bd030c4962831389c5
altaf <altaf98khan66@gmail.com>
2023-04-12 17:59:19 +0530

    Add: To validate Email entered by User

--- src/main/java/com/bridgelabz/userregistrationproblem/UserRegistrationProblem.java
+++ src/main/java/com/bridgelabz/userregistrationproblem/UserRegistrationProblem.java
@@ -7,20 +7,21 @@ import java.util.regex.Pattern;
 public class UserRegistrationProblem {
 
 	public static void main(String[] args) {
-		String regex = "^[A-Z][a-zA-Z]{2,}$";
+
+		String regex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
 
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter The Last Name : ");
-		String lastName = sc.nextLine();
+		System.out.println("Enter Email Address: ");
+		String data = sc.nextLine();
 
 		Pattern pattern = Pattern.compile(regex);
-
-		Matcher matcher = pattern.matcher(lastName);
+		Matcher matcher = pattern.matcher(data);
 		boolean result = matcher.matches();
+
 		if (result) {
-			System.out.println("Valid first name and last name");
+			System.out.println("Valid Email");
 		} else {
-			System.out.println("Pattern not matched");
+			System.out.println("Invalid Email");
 		}
 
 	}
