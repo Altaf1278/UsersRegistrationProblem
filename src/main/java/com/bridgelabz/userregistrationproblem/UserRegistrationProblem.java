6ec5af7e2f31b7a5e3b47ae69c541e9d09e97dc7
altaf <altaf98khan66@gmail.com>
2023-04-12 22:53:17 +0530

    Add: To validate password having atleast 1 special character

--- src/main/java/com/bridgelabz/userregistrationproblem/UserRegistrationProblem.java
+++ src/main/java/com/bridgelabz/userregistrationproblem/UserRegistrationProblem.java
@@ -8,16 +8,13 @@ public class UserRegistrationProblem {
 
 	public static void main(String[] args) {
 
-		String regex = "^(?=.*[A-Z])(?=.*[0-9]).{8,}$";
-
+		String regex = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&+=]).{8,}$";
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter a password having atleast one uppercase & one number : ");
+		System.out.println("Enter a password having atleast 1 uppercase, 1 number & 1 special character  : ");
 		String password = sc.nextLine();
-
 		Pattern pattern = Pattern.compile(regex);
 		Matcher matcher = pattern.matcher(password);
 		boolean result = matcher.matches();
-
 		if (result) {
 			System.out.println("You have entered a Valid Password");
 		} else {
